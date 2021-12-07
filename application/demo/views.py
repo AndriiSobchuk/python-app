f5e4584c336d21c4349f1cab4013526fccb2736b
AndriiSobchuk <86925275+AndriiSobchuk@users.noreply.github.com>
2021-12-07 12:18:29 +0200

    Update views.py

--- application/demo/views.py
+++ application/demo/views.py
@@ -15,7 +15,7 @@ async def health(request: web.Request):
 
 async def app_info(request: web.Request):
     return web.Response(
-        body="Hostname: {}. Version: {}".format(
+        body="Hostname my app: {}. Version: {}".format(
             HOSTNAME,
             VERSION,
         )
